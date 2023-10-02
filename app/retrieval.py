99a018fd3a8d079d840f5075fd9f1f3dfdb06984
devkyle4 <devkyle4@gmail.com>
2023-10-02 18:28:14 +0000

    small changes

--- app/retrieval.py
+++ app/retrieval.py
@@ -62,7 +62,7 @@ def evaluation(user_queries):
         questions = [line.strip() for line in file]
 
     # SAVING THE EVALUATION IN evaluation.csv
-    with open('../docs/evaluation_rated.csv', 'w', newline='', encoding='utf-8') as file:
+    with open('../docs/evaluation.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
 
         writer.writerow([' Question', 'Passage 1', 'Relevance Score 1',
@@ -70,10 +70,13 @@ def evaluation(user_queries):
                          'Passage 2', 'Relevance Score 2', 'Passage 2 Metadata',
                          'Is Passage 2 Relevant? (Yes/No)', 'Passage 3',
                          'Relevance Score 3', 'Passage 3 Metadata', 'Is Passage 3 Relevant? (Yes/No)'])
+
         for question in questions:
             question_embedding = compute_question_embedding(question)
             results = retrieve_relevant_passages(question_embedding)
             row = [question]
+            print(row)
+            exit()
             for hit in results:
                 passage = hit['_source']['Passage']
                 score = hit['_score']
@@ -82,8 +85,24 @@ def evaluation(user_queries):
             writer.writerow(row)
 
 
-evaluation('../docs/user_queries.txt')
+# ACCURACY FOR TOP 1 AND TOP 3
+def compute_accuracies(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
 
-# QUESTION = 'What is the name of the plaintiff?'
-# QUESTION_EMB = compute_question_embedding(QUESTION)
-# print(save_to_csv(QUESTION))
+        relevant_count_top1 = 0
+        relevant_count_top3 = 0
+        total_queries = 0
+
+        for row in reader:
+            print(row[4])
+            exit()
+
+    return ' '
+
+
+# FILENAME = '../docs/evaluation_rated.csv'
+# compute_accuracies(FILENAME)
+
+evaluation('../docs/user_queries.txt')
